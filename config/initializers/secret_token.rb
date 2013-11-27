b090b21943d9afd41946973b35f152b9e84a7ede
nirmesh patel <nirmeshptl@gmail.com>
2013-11-26 19:28:24 -0700

    new startup project on RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startupproject::Application.config.secret_token = 'ea36d8e410384c410a50d450cb51276c32f00f17466e0e2f9be4cb32a06eb9d17fd921cb287acc61042f9fbd540743ff2328e716251718613443fa682e74aa5c'
